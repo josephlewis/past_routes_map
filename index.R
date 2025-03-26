bf81f3c8d62c77eadec874752ad701e8c8beea55
Joseph Lewis <jl2094@cam.ac.uk>
2025-03-26 16:21:30 +0000

    index

--- index.R
+++ index.R
@@ -7,7 +7,8 @@ library(dplyr)
 shapefiles <- list.files("./Data/", pattern = "\\.(shp|gpkg)$", full.names = TRUE, recursive = TRUE)
 
 # Define layer names (adjust these manually to match your files)
-layer_names <- c("Bronze and Iron Age hollow ways, Khabur Valley, Mesopotamia (Priß et al. 2024)", "Qhapaq Ñan - Camino Inca  www.geogpsperu.com", "Roman roads in Wales (Roman Frontiers in Wales and the Marches, 2010. Digitised by Joseph Lewis)")
+layer_names <- c("Bronze and Iron Age hollow ways, Khabur Valley, Mesopotamia", "Qhapaq Ñan - Camino Inca", "Roman roads in Wales")
+layer_names_text <- c("Priß, D., Wainwright, J., Lawrence, D., Turnbull, L., Prell, C., Karittevlis, C., Ioannides, A.A., 2025. Filling the Gaps—Computational Approaches to Incomplete Archaeological Networks. J Archaeol Method Theory 32, 19. https://doi.org/10.1007/s10816-024-09688-z", "www.geogpsperu.com", "Burnham, B.C., Davies, J.L. (Eds.), 2010. Roman Frontiers in Wales and the Marches. Royal Commission on the Ancient and Historical Monuments of Wales, Aberystwyth")
 
 shapes <- list()
 
@@ -21,6 +22,7 @@ for(i in 1:length(shapefiles)) {
   shapes[[i]] <- sf::st_cast(shapes[[i]], "LINESTRING")
   shapes[[i]] <- st_transform(shapes[[i]], crs = st_crs(4326))
   sf::st_geometry(shapes[[i]]) <- "geometry"
+  shapes[[i]]$layer_names_text <- layer_names_text[i]
 }
 
 # Create Leaflet Map
@@ -34,7 +36,8 @@ for (i in seq_along(shapes)) {
                  color = "black", 
                  weight = 1, 
                  opacity = 1, 
-                 group = layer_names[i])
+                 group = layer_names[i],
+                 popup = ~layer_names_text)
 }
 
 # Add layer control
