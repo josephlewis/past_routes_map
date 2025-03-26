f16b659d10bf40899be3e11650c3870c80ccd5ff
Joseph Lewis <jl2094@cam.ac.uk>
2025-03-26 15:48:05 +0000

    files

---
+++ index.R
@@ -0,0 +1,25 @@
+library(leaflet)
+library(htmlwidgets)
+library(sf)
+library(dplyr)
+
+# List all shapefiles and geopackage files in the .Data/ folder
+shapefiles <- list.files("./Data/", pattern = "\\.(shp|gpkg)$", full.names = TRUE, recursive = TRUE)
+
+# Read all files into a list
+shapes <- lapply(shapefiles, st_read)
+
+for(i in 1:length(shapes)) { 
+  
+  shapes[[i]] <- sf::st_transform(shapes[[i]], crs = sf::st_crs(4326))
+  
+}
+
+combined_shapes <- do.call(rbind, shapes)
+
+# Create Leaflet Map
+map <- leaflet() %>%
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  addPolylines(data = combined_shapes, color = "black", weight = 1, fillOpacity = 1)
+
+saveWidget(map, file="index.html")
